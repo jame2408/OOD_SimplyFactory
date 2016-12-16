53bda98b514ad384dda07b8553162c2324a8cda1
jame2408 <jame2408@gmail.com>
2016-12-16 22:24:47 +0800

    Initial Project

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Calculator
 {
@@ -10,6 +6,41 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("請輸入第一個數字：");
+            double firstNumber = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("請輸入運算元(+-*/)：");
+            string operate = Console.ReadLine();
+
+            Console.WriteLine("請輸入第二個數字：");
+            double secondNumber = Convert.ToDouble(Console.ReadLine());
+
+            string result = string.Empty;
+            switch (operate)
+            {
+                case "+":
+                    result = (firstNumber + secondNumber).ToString();
+                    break;
+                case "-":
+                    result = (firstNumber - secondNumber).ToString();
+                    break;
+                case "*":
+                    result = (firstNumber * secondNumber).ToString();
+                    break;
+                case "/":
+                    if (secondNumber != 0)
+                    {
+                        result = (firstNumber / secondNumber).ToString();
+                    }
+                    else
+                    {
+                        result = "不能除以0";
+                    }
+                    break;
+            }
+
+            Console.WriteLine("計算結果：" + result);
+
         }
     }
 }
